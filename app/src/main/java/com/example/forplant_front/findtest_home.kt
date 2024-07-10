90b460b41484bdf97b39712839ae74034bf002ef
sinyoung6491 <sinyoung6491@gmail.com>
2024-07-10 22:01:22 +0900

    home-add_page_first

---
+++ app/src/main/java/com/example/forplant_front/findtest_home.kt
@@ -0,0 +1,11 @@
+package com.example.forplant_front
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class findtest_home : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_findtest_home)
+    }
+}
