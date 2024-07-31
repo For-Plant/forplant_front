c25dd0728b78b892fea311e73f4a11e69db741aa
Jihyun <hjh20030615@gmail.com>
2024-07-31 23:04:46 +0900

    record/bottomSheet

---
+++ app/src/main/java/com/example/forplant_front/ModalBottomSheet.kt
@@ -0,0 +1,22 @@
+package com.example.forplant_front
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+class ModalBottomSheet : BottomSheetDialogFragment()  {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        super.onCreateView(inflater, container, savedInstanceState)
+        return inflater.inflate(R.layout.record_bottomsheet, container, false)
+    }
+
+    companion object {
+        const val TAG = "BasicBottomModalSheet"
+    }
+}
