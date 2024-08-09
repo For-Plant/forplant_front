03479a396e64058f42b5bee7946c927fb9ed18d0
Jihyun <hjh20030615@gmail.com>
2024-08-09 23:01:56 +0900

    record/bottomSheetAndRecord

---
+++ app/src/main/java/com/example/forplant_front/RecordAddPlantFragment.kt
@@ -0,0 +1,11 @@
+package com.example.forplant_front
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+
+class RecordAddPlantFragment: Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
--- app/src/main/java/com/example/forplant_front/RecordBottomSheet.kt
+++ app/src/main/java/com/example/forplant_front/RecordBottomSheet.kt
@@ -6,7 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
-class ModalBottomSheet : BottomSheetDialogFragment()  {
+class RecordBottomSheet : BottomSheetDialogFragment()  {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
