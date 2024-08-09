2d5252e97133d42d294d55940102b11b2df4e295
DESKTOP-FSRVJ63\gram <20kimkayeon@gmail.com>
2024-08-09 22:29:35 +0900

    id 찾기, 비번 재설정 연결 완

--- app/src/main/java/com/example/forplant_front/IdfoundActivity.kt
+++ app/src/main/java/com/example/forplant_front/IdfoundActivity.kt
@@ -2,10 +2,17 @@ package com.example.forplant_front
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.forplant_front.connection.RetrofitClient2
 import com.example.forplant_front.databinding.ActivityAgree2Binding
 import com.example.forplant_front.databinding.ActivityAgree3Binding
 import com.example.forplant_front.databinding.ActivityIdfoundBinding
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 
 class IdfoundActivity : AppCompatActivity() {
@@ -14,12 +21,45 @@ class IdfoundActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityIdfoundBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+        binding.notshowinglay.visibility= View.GONE
+        binding.textView72.visibility= View.GONE
         binding.imageView2.setOnClickListener {
             val intent = Intent(this, LoginActivity::class.java).apply {
                 flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
             }
             startActivity(intent)
+        }
+
+        binding.button.setOnClickListener {
+            val phonenum = binding.Idedittext.text.toString()
+            val username = binding.Idedittext2.text.toString()
+
+            val call = RetrofitObject.getRetrofitService.findid(RetrofitClient2.Requestfindid(phonenum, username))
+            call.enqueue(object : Callback<RetrofitClient2.Responsefindid> {
+                override fun onResponse(call: Call<RetrofitClient2.Responsefindid>, response: Response<RetrofitClient2.Responsefindid>) {
+                    Log.d("Retrofit21", response.toString())
+                    if (response.isSuccessful) {
+                        val response = response.body()
+                        Log.d("Retrofit2", response.toString())
+                        if(response != null){
+                            if(response.isSuccess) {
+                                binding.textView72.visibility= View.GONE
+                                val member_id = response.result.member_id
+                                binding.textView49.text = member_id
+                                binding.notshowinglay.visibility= View.VISIBLE
+                            }else{
+                                binding.notshowinglay.visibility= View.GONE
+                                binding.textView72.visibility= View.VISIBLE
+                            }
+                        }
+                    }
+                }
+
+                override fun onFailure(call: Call<RetrofitClient2.Responsefindid>, t: Throwable) {
+                    val errorMessage = "Call Failed: ${t.message}"
+                    Log.d("Retrofit", errorMessage)
+                }
+            })
         }
 
         binding.textView44.setOnClickListener {
