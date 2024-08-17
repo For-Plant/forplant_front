74ef73de2d600cd5824d57ced41a4c264695f14e
sinyoung6491 <sinyoung6491@gmail.com>
2024-08-17 23:06:25 +0900

    api connect home_1

--- app/src/main/java/com/example/forplant_front/connection/RetrofitAPI.kt
+++ app/src/main/java/com/example/forplant_front/connection/RetrofitAPI.kt
@@ -1,9 +1,7 @@
 package com.example.forplant_front.connection
 
 import retrofit2.Call
-import retrofit2.http.Body
-import retrofit2.http.POST
-import retrofit2.http.PUT
+import retrofit2.http.*
 
 interface RetrofitAPI {
     @POST("/user/login")
@@ -14,4 +12,21 @@ interface RetrofitAPI {
 
     @PUT("/user/change-pw")
     fun changepw(@Body request: RetrofitClient2.Requestchangepw): Call<RetrofitClient2.Responsechangepw>
+
+    @GET("/home/homescreen")
+    fun getHomeScreen(
+        @Header("x-access-token") token: String
+    ): Call<RetrofitClient2.HomeResponse>
+
+    @GET("/home/question")
+    fun getQuestion(
+        @Header("x-access-token") token: String,
+        @Query("num") num: Int
+    ): Call<RetrofitClient2.findtestchoiceResponse>
+
+    @POST("/home/soulmate-result")
+    fun getSoulmateResult(
+        @Header("x-access-token") token: String,
+        @Body answers: Map<String, Int>
+    ): Call<RetrofitClient2.findtestresultResponse>
 }
