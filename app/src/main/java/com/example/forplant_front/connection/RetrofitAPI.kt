09120c4cf2760e0dd5279b1dac35a1b0065d97fa
Jihyun <hjh20030615@gmail.com>
2024-08-17 23:05:59 +0900

    record/data class 생성

--- app/src/main/java/com/example/forplant_front/connection/RetrofitAPI.kt
+++ app/src/main/java/com/example/forplant_front/connection/RetrofitAPI.kt
@@ -2,6 +2,8 @@ package com.example.forplant_front.connection
 
 import retrofit2.Call
 import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.Header
 import retrofit2.http.POST
 import retrofit2.http.PUT
 
@@ -14,4 +16,10 @@ interface RetrofitAPI {
 
     @PUT("/user/change-pw")
     fun changepw(@Body request: RetrofitClient2.Requestchangepw): Call<RetrofitClient2.Responsechangepw>
+
+    @GET("/record/plant-list")
+    fun plantlist(): Call<RetrofitClient2.ResponsePlantlist>
+
+    @GET("/record/record-list?plant_nickname={식물별명}")
+    fun writeplantrecord(): Call<RetrofitClient2.ResponseWriteplant>
 }
